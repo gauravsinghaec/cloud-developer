7a817728862a3fd3ac682b7fc9c36a698ae82bbe
Gaurav Kumar Singh <gauravsinghaec@gmail.com>
2022-12-18 23:55:14 +0530

    add comment for config

--- course-04/exercises/lesson-5/client/src/config.ts
+++ course-04/exercises/lesson-5/client/src/config.ts
@@ -1,7 +1,7 @@
-export const apiEndpoint = '...'
+export const apiEndpoint = 'https://2qn24jse0e.execute-api.us-east-1.amazonaws.com/dev'
 
 export const authConfig = {
-  domain: '...',
-  clientId: '...',
+  domain: '...',  // From OAuth application
+  clientId: '...', // From OAuth application
   callbackUrl: 'http://localhost:3000/callback'
 }
