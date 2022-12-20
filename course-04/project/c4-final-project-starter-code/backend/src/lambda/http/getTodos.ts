81f16081d25cc2f1345b74dfd07a21fc71cb73b0
Gaurav Kumar Singh <gauravsinghaec@gmail.com>
2022-12-20 17:30:42 +0530

    feat: add logger in getTodos lambda handler

--- course-04/project/c4-final-project-starter-code/backend/src/lambda/http/getTodos.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/http/getTodos.ts
@@ -6,10 +6,14 @@ import { cors } from 'middy/middlewares'
 
 import { getTodosForUser as getTodosForUser } from '../../businessLogic/todos'
 import { getUserId } from '../utils'
+import { createLogger } from '../../utils/logger'
+
+const logger = createLogger('getTodos')
 
 export const handler = middy(
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     const userId = getUserId(event)
+    logger.info('userid from token', userId)
     const todos = await getTodosForUser(userId)
 
     return {
